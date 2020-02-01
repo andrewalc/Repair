c7b6ac382d1f6b08fe670a902e779e32d1995cd0
Andrew Alcala <andrewalc98@gmail.com>
2020-02-01 16:05:56 -0500

    Cam move forward and backward is correct

--- Assets/Scripts/CameraShift.cs
+++ Assets/Scripts/CameraShift.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class CameraShift : MonoBehaviour
 {
-    public float shiftX;
+    public float unitShift;
 
     // Start is called before the first frame update
     void Start()
@@ -15,15 +15,24 @@ public class CameraShift : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        // left click
+        if (Input.GetMouseButtonDown(0))
+        {
+            transform.GetComponent<Animator>().Play("CabinCamForward");
+        }
+        // right click
         if (Input.GetMouseButtonDown(1))
         {
-            print("right click");
-            transform.GetComponent<Animator>().Play("CabinCam");
+            transform.GetComponent<Animator>().Play("CabinCamBackward");
         }
     }
 
-    public void shiftPosition()
+    public void shiftForward()
     {
-        transform.parent.position = new Vector3(transform.parent.position.x + shiftX, transform.parent.position.y, transform.parent.position.z);
+        transform.parent.position = new Vector3(transform.parent.position.x - unitShift, transform.parent.position.y, transform.parent.position.z);
+    }
+    public void shiftBackward()
+    {
+        transform.parent.position = new Vector3(transform.parent.position.x + unitShift, transform.parent.position.y, transform.parent.position.z);
     }
 }
