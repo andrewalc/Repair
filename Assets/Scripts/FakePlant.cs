8d280c6d13c5892641050fc0003f23db1eced627
RogerChen98 <chen.rog@husky.neu.edu>
2020-02-01 17:00:38 -0500

    event stuff

---
+++ Assets/Scripts/FakePlant.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class FakePlant : MonoBehaviour
+{
+    void Start()
+    {
+        Tick.Instance.AddEventListener(addWater);
+    }
+
+    void addWater()
+    {
+        WaterResource.Increment();
+        print(WaterResource.value);
+    }
+}
---
+++ Assets/Scripts/Tick.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using UnityEngine;
+
+public class Tick : MonoBehaviour
+{
+    private float _timePerTick;
+    private float _timeUntilTick;
+    private event Action TickEvent;
+    
+    public static Tick Instance { get; private set; }
+
+    private void Start()
+    {
+        _timePerTick = 2;
+        _timeUntilTick = _timePerTick;
+    }
+
+    public void FixedUpdate()
+    {
+        _timeUntilTick -= Time.deltaTime;
+
+        if (_timeUntilTick < 0)
+        {
+            _timeUntilTick += _timePerTick;
+            if (TickEvent != null)
+            {
+                TickEvent();
+            }
+        }
+    }
+
+    public void AddEventListener(Action action)
+    {
+        TickEvent += action;
+    }
+
+    public void RemoveEventListener(Action action)
+    {
+        TickEvent -= action;
+    }
+    
+    void Awake() {
+        if (Instance == null) {
+            Instance = this;
+        } else {
+            Debug.LogError("Can only have one game instance!");
+        }
+    }
+}
