6f9804e46553e71bb3e432c756f032d2bd42f21b
Graham Pentheny <graham.pentheny@gmail.com>
2020-02-01 12:37:44 -0500

    Simplified CarGrid

---
+++ Assets/Scripts/Simulation/CarGrid.cs
@@ -0,0 +1,11 @@
+﻿public class CarGrid {
+    public GridSquare[,] Squares;
+    
+    public CarGrid(int width, int height) {
+        for (int x = 0; x < width; ++x) {
+            for (int y = 0; y < height; ++y) {
+                Squares[x, y] = new GridSquare();
+            }
+        }
+    }
+}
