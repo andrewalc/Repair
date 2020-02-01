f00475c582b37e321e06b82ef7882e9501615c80
Graham Pentheny <graham.pentheny@gmail.com>
2020-02-01 12:59:11 -0500

    Car object types

---
+++ Assets/Scripts/Simulation/CarObjects/EmptyCarObject.cs
@@ -0,0 +1,11 @@
+﻿public class EmptyCarObject : ICarObject {
+    public CarObjectType Type => CarObjectType.Empty;
+    
+    public float GetPlantGrowthModifier() {
+        throw new System.NotImplementedException();
+    }
+
+    public bool BlocksIrrigation() { return false; }
+    public bool IsEmpty() { return true; }
+    public bool IsWaterSource() { return false; }
+}
---
+++ Assets/Scripts/Simulation/CarObjects/ObstacleCarObject.cs
@@ -0,0 +1,11 @@
+﻿public class ObstacleCarObject : ICarObject {
+    public CarObjectType Type => CarObjectType.Obstacle;
+
+    public float GetPlantGrowthModifier() {
+        throw new System.NotImplementedException();
+    }
+
+    public bool BlocksIrrigation() { return true; }
+    public bool IsEmpty() { return false; }
+    public bool IsWaterSource() { return false; }
+}
