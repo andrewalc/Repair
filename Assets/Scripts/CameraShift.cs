49cac0985549132e461098c3f8689a0816f73229
Andrew Alcala <andrewalc98@gmail.com>
2020-02-01 14:37:46 -0500

    Right click to move the camera over

---
+++ Assets/Scripts/CameraShift.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraShift : MonoBehaviour
+{
+    public float shiftX;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(1))
+        {
+            print("right click");
+            transform.GetComponent<Animator>().Play("CabinCam");
+        }
+    }
+
+    public void shiftPosition()
+    {
+        transform.parent.position = new Vector3(transform.parent.position.x + shiftX, transform.parent.position.y, transform.parent.position.z);
+    }
+}
---
+++ Assets/Scripts/TimelinePlayer.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Playables;
+using UnityEngine.Timeline;
+
+public class TimelinePlayer : MonoBehaviour
+{
+    public TimelineAsset asset;
+    public PlayableDirector director;
+
+    public bool playOnce;
+    private bool hasPlayedOnce;
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (director == null)
+        {
+            Debug.LogError("No timeline director found!");
+        }
+    }
+
+    public void PlayAsset()
+    {
+        if (playOnce && hasPlayedOnce) return; // Play the timeline asset once if flag set
+        print("playing asset");
+        director.playableAsset = asset;
+        //rebuild for runtime playing
+        director.RebuildGraph();
+        director.time = 0.0;
+        director.Play();
+        hasPlayedOnce = true;
+    }
+}
