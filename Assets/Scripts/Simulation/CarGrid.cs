1bcd534209b1a1c2f81c6cfc4bcf4b51b534ddf4
Graham Pentheny <graham.pentheny@gmail.com>
2020-02-01 13:25:57 -0500

    Fixed error in grid

--- Assets/Scripts/Simulation/CarGrid.cs
+++ Assets/Scripts/Simulation/CarGrid.cs
@@ -1,10 +1,12 @@
-﻿public class CarGrid {
+﻿
+
+public class CarGrid {
     public GridSquare[,] Squares;
     
     public CarGrid(int width, int height) {
         for (int x = 0; x < width; ++x) {
             for (int y = 0; y < height; ++y) {
-                Squares[x, y] = new GridSquare();
+                Squares[x, y] = new GridSquare(x, y);
             }
         }
     }
