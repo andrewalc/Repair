d492d5388d49d553e202e8e81b0500ae9bbe515b
Graham Pentheny <graham.pentheny@gmail.com>
2020-02-01 15:20:57 -0500

    Simulation settings config

---
+++ Assets/Scripts/Configs/SimulationSettingsConfig.cs
@@ -0,0 +1,6 @@
+﻿using System.Collections.Generic;
+
+[ConfigMandatory]
+public class SimulationSettingsConfig {
+    public Dictionary<string, CarGrid> testGrids = new Dictionary<string, CarGrid>();
+}
--- Assets/Scripts/Simulation/CarGrid.cs
+++ Assets/Scripts/Simulation/CarGrid.cs
@@ -1,13 +1,46 @@
-﻿
+﻿using System;
+using DarkConfig;
 
 public class CarGrid {
     public GridSquare[,] Squares;
-    
+
     public CarGrid(int width, int height) {
+        Squares = new GridSquare[width, height];
         for (int x = 0; x < width; ++x) {
             for (int y = 0; y < height; ++y) {
                 Squares[x, y] = new GridSquare(x, y);
             }
         }
+    }
+
+    public static CarGrid FromDoc(CarGrid existing, DocNode doc) {
+        var docString = doc.AsString();
+        int width = docString.IndexOf('\n');
+
+        var lines = docString.Split('\n');
+        int height = lines.Length;
+        
+        existing = new CarGrid(width, height);
+        
+        for (int x = 0; x < width; ++x) {
+            for (int y = 0; y < height; ++y) {
+                ICarObject carObject;
+                
+                // lines is row=major, but we want column-major for the grid.
+                var c = lines[height - y - 1][x];
+                switch (c) {
+                    case '.': carObject = new EmptyCarObject(); break;
+                    case 'p': carObject = new PlantCarObject(); break;
+                    case 'm': carObject = new MachineCarObject(); break;
+                    case 'o': carObject = new ObstacleCarObject(); break;
+                    case 's': carObject = new ObstacleCarObject(); break;
+                    default: throw new ArgumentOutOfRangeException();
+                }
+                existing.Squares[x, y] = new GridSquare(x, y) {
+                    ContainedObject = carObject
+                };
+            }
+        }
+        return existing;
     }
 }
