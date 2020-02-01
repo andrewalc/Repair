92ce37072c1e466d49ed5af9fc27f82e0ac07e10
Andrew Alcala <andrewalc98@gmail.com>
2020-01-31 20:36:18 -0500

    Starting Click Manager script

---
+++ Assets/Scripts/ClickManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Cinemachine;
+using UnityEngine;
+
+public class ClickManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Camera cam;
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 screenPos = cam.ScreenToWorldPoint(target.position);
+        Debug.Log("target is " + screenPos.x + " pixels from the left");
+    }
+}
