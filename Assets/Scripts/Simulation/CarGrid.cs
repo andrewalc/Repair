fa5050bcbe95329aeb406bf38f624c4e4ee9cf6b
Graham Pentheny <graham.pentheny@gmail.com>
2020-02-01 15:31:58 -0500

    Can clone simulation state objects

--- Assets/Scripts/Simulation/CarGrid.cs
+++ Assets/Scripts/Simulation/CarGrid.cs
@@ -13,6 +13,18 @@ public class CarGrid {
         }
     }
 
+    public CarGrid Clone() {
+        var clone = new CarGrid(Squares.GetLength(0), Squares.GetLength(1));
+
+        for (int x = 0; x < Squares.GetLength(0); ++x) {
+            for (int y = 0; y < Squares.GetLength(1); ++y) {
+                clone.Squares[x,y] = Squares[x, y].Clone();
+            }
+        }
+        
+        return clone;
+    }
+
     public static CarGrid FromDoc(CarGrid existing, DocNode doc) {
         var docString = doc.AsString();
         int width = docString.IndexOf('\n');
@@ -33,7 +45,7 @@ public class CarGrid {
                     case 'p': carObject = new PlantCarObject(); break;
                     case 'm': carObject = new MachineCarObject(); break;
                     case 'o': carObject = new ObstacleCarObject(); break;
-                    case 's': carObject = new ObstacleCarObject(); break;
+                    case 's': carObject = new SpigotCarObject(); break;
                     default: throw new ArgumentOutOfRangeException();
                 }
                 existing.Squares[x, y] = new GridSquare(x, y) {
--- Assets/Scripts/Simulation/CarObjects/PlantCarObject.cs
+++ Assets/Scripts/Simulation/CarObjects/PlantCarObject.cs
@@ -8,4 +8,5 @@
     public bool BlocksIrrigation() { return false; }
     public bool IsEmpty() { return false; }
     public bool IsWaterSource() { return false; }
+    public ICarObject Clone() { return new PlantCarObject(); }
 }
