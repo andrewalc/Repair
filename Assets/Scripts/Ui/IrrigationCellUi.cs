075a4a19e87c2e2b0c123815836d1f952ca936d4
Andrew Alcala <andrewalc98@gmail.com>
2020-02-01 22:27:31 -0500

    Irrigation Cell Generation

---
+++ Assets/Scripts/Ui/IrrigationCellUi.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class IrrigationCellUi : MonoBehaviour
+{
+    public GridSquare square;
+
+    private Image img;
+
+    public StringToSprite[] spriteMap;
+    [System.Serializable] 
+    
+    public class StringToSprite
+    {
+        /// Name of the variable
+        public string name;
+        /// Value of the variable
+        public Sprite sprite;
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        img = GetComponent<Image>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        Render();
+    }
+
+    void Render()
+    {
+        string name = "";
+        switch (square.ContainedObject.Type)
+        {
+            case CarObjectType.Machine: {}
+                name = "machine";
+                break;
+            case CarObjectType.Obstacle:
+                name = "obstacle";
+                break;
+            case CarObjectType.Plant:
+                name = "plant";
+                break;
+            case CarObjectType.Spigot:
+                name = "spigot";
+                break;
+            case CarObjectType.Empty:
+                name = "pipe";
+                break;
+            default:
+                name = "sprinkler";
+                break;
+        }
+        foreach (StringToSprite sts in spriteMap)
+        {
+            if (sts.name != name) continue;
+            img.sprite = sts.sprite;
+            break;
+        }
+    }
+}
---
+++ Assets/Scripts/Ui/IrrigationCellUiGen.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IrrigationCellUiGen : MonoBehaviour
+{
+    public GameObject CellPrefab;
+    public bool updated;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        updated = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!updated && Game.Instance.finishedGeneratingLevel)
+        {
+            UpdateCellGrid();
+            updated = true;
+        }
+
+    }
+
+    public void UpdateCellGrid()
+    {
+        CarGrid grid = Game.Instance.currGrid;
+        for (int x = 0; x < grid.Squares.GetLength(0); ++x) {
+            for (int y = 0; y < grid.Squares.GetLength(1); ++y)
+            {
+                GameObject button = Instantiate(CellPrefab, new Vector3(0, 0, 0), Quaternion.identity);
+                IrrigationCellUi cell = button.GetComponent<IrrigationCellUi>();
+                cell.square = grid.Squares[x, y].Clone();
+                button.transform.parent = transform;
+            }
+        }
+    }
+}
