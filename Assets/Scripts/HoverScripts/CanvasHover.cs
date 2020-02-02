6a5f4098a01adfeca0cb6bf890f08897c02e6752
RogerChen98 <chen.rog@husky.neu.edu>
2020-02-02 12:23:02 -0500

    initial hover script stuff

---
+++ Assets/Scripts/HoverScripts/CanvasHover.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Experimental.PlayerLoop;
+
+public class CanvasHover : MonoBehaviour
+{
+    void FixedUpdate()
+    {
+        transform.position = Input.mousePosition;
+    }
+}
