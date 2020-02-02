857d44ed0ace03bb1a6be44ea46b95f58ac12f65
Andrew Alcala <andrewalc98@gmail.com>
2020-02-02 16:30:20 -0500

    Removed click to move

--- Assets/TrainGenerator.cs
+++ Assets/TrainGenerator.cs
@@ -45,16 +45,16 @@ public class TrainGenerator : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetMouseButtonDown(0))
-        {
-            CreateTrainCar();
-            FindObjectOfType<CameraShift>().AnimateForward();
-        }
-
-        if (Input.GetMouseButtonDown(1))
-        {
-            FindObjectOfType<CameraShift>().AnimateBackward();
-        }
+//        if (Input.GetMouseButtonDown(0))
+//        {
+//            CreateTrainCar();
+//            FindObjectOfType<CameraShift>().AnimateForward();
+//        }
+//
+//        if (Input.GetMouseButtonDown(1))
+//        {
+//            FindObjectOfType<CameraShift>().AnimateBackward();
+//        }
     }
 
     public void CreateTrainCar()
