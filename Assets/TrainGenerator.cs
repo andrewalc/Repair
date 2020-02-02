f5cd1cbe44a4b468024d0e07a2adf574f9ccefbf
Andrew Alcala <andrewalc98@gmail.com>
2020-02-02 15:47:02 -0500

    TrainGenerator cool

--- Assets/TrainGenerator.cs
+++ Assets/TrainGenerator.cs
@@ -8,6 +8,9 @@ public class TrainGenerator : MonoBehaviour
     public GameObject startingTrain;
     private List<GameObject> trains;
     public int currentTrainIndex;
+
+    public bool generated;
+    public bool firstCar;
     // Start is called before the first frame update
     void Start()
     {
@@ -17,22 +20,48 @@ public class TrainGenerator : MonoBehaviour
         }
         trains = new List<GameObject>();
         trains.Add(startingTrain);
-        CreateTrainCar(); // Have one in advance
+        StartCoroutine(GenFirstCar());
+        
     }
 
+
+    IEnumerator GenFirstCar()
+    {
+        yield return null;
+        GenerateLevel(0);
+        yield return new WaitUntil( () => Game.Instance.finishedGeneratingLevel);
+        CreateTrainCar();
+        while (true)
+        {
+            yield return null;
+            if (!generated && Game.Instance.finishedGeneratingLevel)
+            {
+            GenerateLevel(trains.Count - 1);
+            generated = true;
+            }
+        }
+    }
+    
     // Update is called once per frame
     void Update()
     {
         if (Input.GetMouseButtonDown(0))
         {
             CreateTrainCar();
+            FindObjectOfType<CameraShift>().AnimateForward();
+        }
+
+        if (Input.GetMouseButtonDown(1))
+        {
+            FindObjectOfType<CameraShift>().AnimateBackward();
         }
     }
 
     public void CreateTrainCar()
     {
+        generated = false;
         Vector3 trainPos = startingTrain.transform.position;
-        print(trains);
+        print(trains.Count);    
         GameObject newTrain = Instantiate(trainPrefab, new Vector3(trainPos.x - (33f * trains.Count), trainPos.y, trainPos.z), Quaternion.identity);
         newTrain.transform.parent = transform;
         trains.Add(newTrain);
