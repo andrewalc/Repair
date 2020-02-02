c96ae9a872d417250f476d0ffe8242824de506cf
RogerChen98 <chen.rog@husky.neu.edu>
2020-02-02 16:03:05 -0500

    menu mock

---
+++ Assets/Scripts/InSceneMenu.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using DG.Tweening;
+using UnityEngine;
+
+public class InSceneMenu : MonoBehaviour
+{
+    public GameObject cam;
+    
+
+    public void Transistion()
+    {
+        cam.SetActive(false);
+        GetComponent<CanvasGroup>()
+            .DOFade(0, 0.5f)
+            .OnComplete(() =>
+            {
+                Destroy(gameObject);
+            });
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+}
---
+++ Assets/Scripts/IrrigationPanelActivation.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IrrigationPanelActivation : MonoBehaviour
+{
+    public GameObject IrrigationPanelPrefab;
+    GameObject IrrigationPanelInstance;
+    
+    void OnMouseOver(){
+        if(Input.GetMouseButtonDown(0))
+        {
+            if (IrrigationPanelInstance != null) {
+                Destroy(IrrigationPanelInstance);
+            } else {
+                IrrigationPanelInstance = Instantiate(IrrigationPanelPrefab);
+                IrrigationPanelInstance.GetComponent<IrrigationUI>().Init();
+            }
+        }
+    }
+}
