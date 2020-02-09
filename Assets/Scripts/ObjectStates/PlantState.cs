a659bbc5f570d97c9777ee31eca6787f803f9030
MarkSavage11 <savagemark7@gmail.com>
2020-02-09 15:22:08 -0500

    Plants now change material based on health

--- Assets/Scripts/ObjectStates/PlantState.cs
+++ Assets/Scripts/ObjectStates/PlantState.cs
@@ -1,16 +1,21 @@
 using System;
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Experimental.PlayerLoop;
 
 public class PlantState : MonoBehaviour
 {
-    public string State;
+    public string State => GetState();
     public GridSquare square;
     private MeshRenderer mesh;
 
     public StringToMaterial[] materialMap;
+	private Dictionary<string, Material> materialDict = new Dictionary<string, Material>();
+
+	private bool listenersSet = false;
+
     [System.Serializable] 
-    
     public class StringToMaterial
     {
         public string name;
@@ -20,15 +25,49 @@ public class PlantState : MonoBehaviour
     void Start()
     {
         mesh = GetComponent<MeshRenderer>();
+		foreach(StringToMaterial stm in materialMap)
+		{
+			materialDict[stm.name] = stm.material;
+		}
     }
 
-    void FixedUpdate()
-    {
-        foreach (StringToMaterial stm in materialMap)
-        {
-            if (stm.name != State) continue;
-            mesh.material = stm.material;
-            break;
-        }
-    }
+	public void Update()
+	{
+		if (!listenersSet)
+		{
+			Tick.Instance.RemoveEventListener(UpdateHealth);
+			Tick.Instance.AddEventListener(UpdateHealth);
+			listenersSet = true;
+		}
+	}
+
+	public void OnDestroy()
+	{
+		Tick.Instance.RemoveEventListener(UpdateHealth);
+		listenersSet = false;
+	}
+
+	string GetState()
+	{
+		if (square.ContainedObject.Type != CarObjectType.Plant) throw new Exception("PlantState does not have a gridsquare with CarObjectType \"Plant\"");
+		var plantObject = (PlantCarObject)square.ContainedObject;
+		//TODO: Make plant states stored in the plant object and an enum
+		print(plantObject.health);
+		if(plantObject.health < 50)
+		{
+			return "sad";
+		}else if(plantObject.health < 100)
+		{
+			return "medium";
+		}
+		else
+		{
+			return "yeet";
+		}
+	}
+
+	void UpdateHealth()
+	{
+		mesh.material = materialDict[State];
+	}
 }
