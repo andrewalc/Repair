65494df2f24f51c3f4af15b953950484090d16a3
NPCDev <npcdev@tuta.io>
2020-02-09 14:36:42 -0500

    Pipe state is now initialized correctly when creating the Irrigation UI.

--- Assets/Scripts/Ui/IrrigationUI.cs
+++ Assets/Scripts/Ui/IrrigationUI.cs
@@ -1,7 +1,8 @@
 ﻿using UnityEngine;
 using UnityEngine.UI;
 
-public class IrrigationUI : MonoBehaviour {
+public class IrrigationUI : MonoBehaviour
+{
     [SerializeField] Text pipeCost;
     [SerializeField] Text sprinklerCost;
 
@@ -10,11 +11,13 @@ public class IrrigationUI : MonoBehaviour {
     [SerializeField] GameObject CellPrefab;
     [SerializeField] GameObject PipePrefab;
 
-    void Awake() {
+    void Awake()
+    {
         HoverManager.Instance.disabled = true;
     }
 
-    void OnDestroy() {
+    void OnDestroy()
+    {
         HoverManager.Instance.disabled = false;
     }
 
@@ -24,25 +27,56 @@ public class IrrigationUI : MonoBehaviour {
         Destroy(gameObject);
     }
 
-    public void Init() {
-        foreach (Transform child in cellGrid.transform) {
+    public void Init()
+    {
+        foreach (Transform child in cellGrid.transform)
+        {
             Destroy(child.gameObject);
         }
-        
+
         CarGrid grid = Game.Instance.Simulation.currentState;
-        for (int y = 0; y < grid.Height; ++y) {
-            for (int x = 0; x < grid.Width; ++x) {
+        for (int y = 0; y < grid.Height; ++y)
+        {
+            for (int x = 0; x < grid.Width; ++x)
+            {
                 var button = Instantiate(CellPrefab, cellGrid.transform);
                 var cell = button.GetComponent<IrrigationCellUi>();
                 cell.Init(grid.Squares[x, y].ContainedObject.Type);
             }
         }
 
-        for (int y = 0; y < grid.Height * 2 - 1; ++y) {
-            for (int x = 0; x < grid.Width * 2 - 1; ++x) {
+        for (int y = 0; y < grid.Height * 2 - 1; ++y)
+        {
+            for (int x = 0; x < grid.Width * 2 - 1; ++x)
+            {
                 var button = Instantiate(PipePrefab, pipeGrid.transform);
 
-                button.GetComponent<PipeButton>().Init(pipeState.Empty, x, y);
+                PipeState state = CalculatePipeState(x, y, grid);
+
+                button.GetComponent<PipeButton>().Init(CalculatePipeState(x, y, grid), x, y);
+            }
+        }
+    }
+
+    private PipeState CalculatePipeState(int x, int y, CarGrid grid)
+    {
+        if (x % 2 == 0 && y % 2 == 0)
+        {
+            return grid.Sprinklers[x / 2, y / 2] ? PipeState.Sprinkler : PipeState.Empty;
+        }
+        else
+        {
+            if (x % 2 == 0)
+            {
+                return (grid.PipeConnections[x / 2, y / 2] & PipeConnection.Top) != 0
+                    ? PipeState.Pipe
+                    : PipeState.Empty;
+            }
+            else
+            {
+                return (grid.PipeConnections[x / 2, y / 2] & PipeConnection.Right) != 0
+                    ? PipeState.Pipe
+                    : PipeState.Empty;
             }
         }
     }
